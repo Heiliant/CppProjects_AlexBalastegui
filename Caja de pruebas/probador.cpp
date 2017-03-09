753710923c54f25ec239ae50214b2e89fcb34dbc
alexbalasteguigaya <alexbalasteguigaya@S3-02>
2017-03-09 14:03:11 +0100

    push it

--- Caja de pruebas/probador.cpp
+++ Caja de pruebas/probador.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-void main() {
+void main2() {
 	int x(10);
 	std::cout << x << std::endl;
 	std::cout << &x << std::endl;
 	std::cout << *&x << std::endl;
 
 	int *y;
+	y = new int{ 4 };
 	std::cout << y;
 }
